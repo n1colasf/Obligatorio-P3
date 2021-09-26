fd53b00711a48d1c76ea19074c935922d57253a4
gonzabazzi <gonzabazzi@gmail.com>
2021-09-26 11:23:05 -0300

    Métodos Fachada

--- Repositorios/FachadaClub.cs
+++ Repositorios/FachadaClub.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,37 @@ namespace Repositorios
 {
     public class FachadaClub
     {
+        //Este atributo va? porque me obliga a agregar el using Dominio
+        //public List<Funcionario> Funcionarios { get; set; }
+
+        public bool LogIn(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+        public bool LogOut()
+        {
+            throw new NotImplementedException();
+        }
+        public bool AnotarseAActividad(Socio socio, Actividad actividad)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Actividad> IngresosPorFecha(int cedula, DateTime fecha)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Actividad> ProximasActividades(DateTime fecha)
+        {
+            throw new NotImplementedException();
+        }
+        public bool RegistrarPago(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void ExportarInformacion()
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
