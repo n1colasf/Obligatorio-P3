63854e046ccb47b225b154e487005166ca4e455c
gonzabazzi <gonzabazzi@gmail.com>
2021-10-12 11:03:12 -0300

    Index inicio

--- MVC_Club/App_Start/RouteConfig.cs
+++ MVC_Club/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC_Club
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Funcionario", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Inicio", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
